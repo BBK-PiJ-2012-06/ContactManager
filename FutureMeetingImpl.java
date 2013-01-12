a71ae194e0f8f349ed191324d8fbea644d8d286f
Simon Ward <simon.ward42@gmail>
2013-01-12 18:18:04 +0000

    initial commit: implements FutureMeeting (including all abstract methods inherited from Meeting interface; similar to PastMeetingImpl I know of no simpler way around this, in any case it just involves a little copy-pasta). Looking at all the separate meeting classes we have, my first-pass solution for managing meeting IDs is going to be very messy at best and liable to ID overloading/overwriting at worst. This should be controlled centrally by the ContactManager itself.

---
+++ FutureMeetingImpl.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * A meeting to be held in the future.
+ *
+ **/
+ public class FutureMeetingIpml implements FutureMeeting {
+	// No methods here, this is just a naming interface
+	// (i.e. only necessary for type checking and/or downcasting)
+ }
