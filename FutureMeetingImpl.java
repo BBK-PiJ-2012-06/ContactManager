69b0d21af0a9ec2f8f5b7292ed33c4d0c1f76aa9
Simon Ward <simon.ward42@gmail>
2013-01-12 20:06:04 +0000

    recommitting FutureMeetingImpl - appears to be an older version of the code on GitHub (possibly forgot to git add??)

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -6,7 +6,59 @@ import java.util.HashSet;
  * A meeting to be held in the future.
  *
  **/
- public class FutureMeetingIpml implements FutureMeeting {
-	// No methods here, this is just a naming interface
+ public class FutureMeetingImpl implements FutureMeeting {
+	// Direct copy of MeetingImpl, this is just a naming class
 	// (i.e. only necessary for type checking and/or downcasting)
+	private int id;
+	private Calendar date = null;
+	private Set<Contact> contacts = new HashSet<Contact>();
+	// nextAvailableId is used to assign IDs to new meetings, is static as it is 
+	// only needed by the class, not objects.
+	private static int nextAvailableId = 0;
+	
+	/**
+	 * Constructs a FutureMeetingImpl with the given set of contacts and date.
+	 *
+	 * @param contacts the set of contacts attending the meeting
+	 * @param date the date on which the meeting is to take place
+	 **/
+	public FutureMeetingImpl(Set<Contact> contacts, Calendar date) {
+		this.contacts = contacts;
+		this.date = date;
+		this.id = nextAvailableId++; // assigns nextAvailableId to id, then increments nextAvailableId.
+	}
+	
+	/**
+	 * Returns the ID of the meeting.
+	 *
+	 * @return the ID of the meeting
+	 **/
+	@Override
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Returns the date of the meeting.
+	 *
+	 * @return the date of the meeting
+	 **/
+	@Override
+	public Calendar getDate() {
+		return date;
+	}
+	
+	/**
+	 * Returns the details of people that attended the meeting.
+	 *
+	 * The list contains a minimum of one contact (if there were
+	 * just two people: the user and the contact) and may contain an 
+	 * arbitrary number of them.
+	 *
+	 * @return the details of people that attended the meeting
+	 **/
+	@Override
+	public Set<Contact> getContacts() {
+		return contacts;
+	}
  }
