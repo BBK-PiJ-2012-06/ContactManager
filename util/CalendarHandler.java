03f88ac75fa2d3ca311aa186ed543d71826a61ee
Simon Ward <simon.ward42@gmail>
2013-01-19 17:44:18 +0000

    Added a date formatting method - returns a String representing the given Calendar in the format "dd/MM/yy HH:mm" (see SimpleDateFormat for pattern letter definitions).

--- util/CalendarHandler.java
+++ util/CalendarHandler.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.util.SimpleDateFormat;
 
 /**
  * A class to handle some common Calendar checking/manipulating methods.
@@ -6,11 +7,44 @@ import java.util.Calendar;
 public class CalendarHandler {
 	
 	/**
-	 * Check if the given date is in the past, namely at a time earlier than 00:00 today
+	 * Checks whether the given date is in the past.
 	 * 
-	 * 
+	 * @param date the date to check 
+	 * @return whether the date is in the past (true) or not (false)
 	 **/
 	public static boolean isInPast(Calendar date) {
+		Calendar now = Calendar.getInstance();
+		if(date.before(now)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	/**
+	 * Checks whether the given date is in the future.
+	 *
+	 * @param date the date to check 
+	 * @return whether the date is in the future (true) or not (false)
+	 **/
+	public static boolean isInFuture(Calendar date) {
+		Calendar now = Calendar.getInstance();
+		if(date.after(now)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Formats the given date to a simple string of the form "dd/MM/yy HH:mm" 
+	 *(see SimpleDateFormat for pattern letter definitions)
+	 *
+	 * @param date the date to be formatted
+	 * @return a string representation of the given date
+	 **/
+	public static String format(Calendar date) {
+		SimpleDateFormat simpleFormat = new SimpleDateFormat("dd/MM/yy HH:mm");
+		return simpleFormat.format(date.getTime());
 	}
 }
