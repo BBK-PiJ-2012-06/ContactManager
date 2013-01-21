f06af67758b0547c59ce3963fe883453992c1c20
Simon Ward <simon.ward42@gmail>
2013-01-21 19:23:32 +0000

    recommit: initial commit was not most recent version

--- ContactImplTest.java
+++ ContactImplTest.java
@@ -2,12 +2,60 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactImplTest {
+	private Contact c1, c2, c2_too;
+	private final String n = "here's a note";
+	private int h1, h2, h2_too;
+	
+	@Before
+	public void buildUp() {
+		c1 = new ContactImpl(1, "Alice", "some notes");
+		c2 = new ContactImpl(2, "Bob");
+		c2_too = new ContactImpl(2, "Bob");
+	}
+	
 	@Test
-	public void testsNormalName() {
-		Person p = new Person();
-		String input = "Dereck Robert Yssirt";
-		String output = p.getInitials(input);
-		String expected = "DRY";
-		assertEquals(output, expected);
+	public void testsGetID() {
+		assertEquals(1, c1.getID());
+		assertEquals(2, c2.getID());
+	}
+	
+	@Test
+	public void testsGetName() {
+		assertEquals("Alice", c1.getName());
+		assertEquals("Bob", c2.getName());
+	}
+	@Test
+	public void testsGetNotes() {
+		assertEquals("some notes", c1.getNotes());
+	}
+	@Test
+	public void testsGetNotesAfterFirstAddNotes() {
+		c2.addNotes(n);
+		assertEquals(n, c2.getNotes());
+	}
+	@Test
+	public void testsGetNotesTwoLines() {
+		c1.addNotes(n);
+		assertEquals("some notes" + "\n" + n, c1.getNotes());
+	}
+	@Test
+	public void testsEquals() {
+		assertTrue(c2.equals(c2_too));
+	}
+	@Test
+	public void testsHash() { 
+		h1 = c1.hashCode();
+		h2 = c2.hashCode(); 
+		h2_too = c2_too.hashCode();	
+		
+		// hashCode()'s contract:
+		//	-- must return the same integer while running, as long as no mutable field involved in equals() changes
+		//	-- if two objects are equal under equals(), they must return the same hashCode()
+		//	-- two unequal objects under equals() are not required to return different hashes, but it is preferable.
+		assertTrue(c1.hashCode() == h1);
+		assertTrue(h1 != h2);
+		assertTrue(h2 == h2_too);
+		c1.addNotes("this should change c1's hash");
+		assertTrue(c1.hashCode() != h1);
 	}
 }
