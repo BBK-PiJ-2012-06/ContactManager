2dfcdbf49e22a97e3c290d1ad66d1f8a0c838358
Simon Ward <simon.ward42@gmail>
2013-01-22 14:31:56 +0000

    added overridden equals and hashCode methods

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -19,4 +19,22 @@ import java.util.HashSet;
 	public FutureMeetingImpl(int id, Set<Contact> contacts, Calendar date) {
 		super(id, contacts, date);
 	}
+	
+	@Override // see http://www.artima.com/lejava/articles/equality.html
+	public boolean equals(Object other) {
+		boolean result = false;
+		if(other != null && other instanceof FutureMeetingImpl) {
+			FutureMeetingImpl that = (FutureMeetingImpl) other;
+			result = (this.getID() == that.getID());
+			result = result && (this.getContacts().equals(that.getContacts()));
+			result = result && (this.getDate().equals(that.getDate())); 
+			result = result && (this.getNotes().equals(that.getNotes()));
+		}
+		return result;
+	}
+	
+	@Override // see link above
+	public int hashCode() {
+		return super.hashCode();
+	}
  }
