d1693076048c9618b8f13a7c78bbde87749130df
Simon Ward <simon.ward42@gmail.com>
2013-02-02 18:24:31 +0000

    added to buildUp()

--- test/util/DataUtilitiesXmlImplTest.java
+++ test/util/DataUtilitiesXmlImplTest.java
@@ -8,11 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import main.Contact;
-import main.ContactImpl;
-import main.FutureMeeting;
-import main.PastMeeting;
-import main.PastMeetingImpl;
+import main.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -28,7 +24,7 @@ public class DataUtilitiesXmlImplTest {
 	private Contact alice, bob;
 	private PastMeeting p1, p2;
 	private FutureMeeting f1, f2;
-	private Calendar past1, past2, past3, past4;
+	private Calendar past1, past2, future1, future2;
 	
 	@Before
 	public void buildUp() {
@@ -41,11 +37,25 @@ public class DataUtilitiesXmlImplTest {
 		contacts = new HashSet<Contact>();
 		contacts.add(alice);
 		contacts.add(bob);
-		
-		past1 = new Calendar
-		p1 = new PastMeetingImpl(1, contacts, "note");
-		p2 = new 
-	}
+
+        past1 = Calendar.getInstance();
+        past1.clear();
+        past1.set(1900, Calendar.JANUARY, 1, 00, 00);
+        past2 = Calendar.getInstance();
+        past2.clear();
+        past2.set(1901, Calendar.JANUARY, 1, 00, 00);
+        p1 = new PastMeetingImpl(1, contacts, past1,  "note");
+		p2 = new PastMeetingImpl(2, contacts, past2, "note");
+
+        future1 = Calendar.getInstance();
+        future1.clear();
+        future1.set(2100, Calendar.JANUARY, 1, 00, 00);
+        future2 = Calendar.getInstance();
+        future2.clear();
+        future2.set(2101, Calendar.JANUARY, 1, 00, 00);
+        f1 = new FutureMeetingImpl(1, contacts, future1);
+        f2 = new FutureMeetingImpl(2, contacts, future2);
+    }
 
 	@After
 	public void cleanUp() {
