fe8d2cf5c8061d5cb43bcf7f823a78be3d7c93c0
BBK-PiJ-2012-06 <simon.ward42@gmail.com>
2013-02-04 17:27:33 +0000

    implemented first set of tests, half of which currently fail due to badly implemented mock methods in DataUtilityXmlImpl

--- test/util/DataUtilitiesXmlImplTest.java
+++ test/util/DataUtilitiesXmlImplTest.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+
+import javax.swing.text.DefaultEditorKit.PasteAction;
 
 import main.*;
 
@@ -16,7 +19,6 @@ import org.junit.Test;
 
 public class DataUtilitiesXmlImplTest {
 	private DataUtilities data;
-	private File file;
 	private String filename;
 	private Set<Contact> contacts;
 	private List<PastMeeting> pastmeetings;
@@ -30,7 +32,6 @@ public class DataUtilitiesXmlImplTest {
 	public void buildUp() {
 		data = new DataUtilitiesXmlImpl();
 		filename = "." + File.separator + "test_file.txt";
-		file = new File(filename);
 		
 		alice = new ContactImpl(1, "Alice");
 		bob = new ContactImpl(2, "Bob");
@@ -46,6 +47,10 @@ public class DataUtilitiesXmlImplTest {
         past2.set(1901, Calendar.JANUARY, 1, 00, 00);
         p1 = new PastMeetingImpl(1, contacts, past1,  "note");
 		p2 = new PastMeetingImpl(2, contacts, past2, "note");
+		
+		pastmeetings = new LinkedList<PastMeeting>();
+		pastmeetings.add(p1);
+		pastmeetings.add(p2);
 
         future1 = Calendar.getInstance();
         future1.clear();
@@ -55,12 +60,58 @@ public class DataUtilitiesXmlImplTest {
         future2.set(2101, Calendar.JANUARY, 1, 00, 00);
         f1 = new FutureMeetingImpl(1, contacts, future1);
         f2 = new FutureMeetingImpl(2, contacts, future2);
+        
+        futuremeetings = new LinkedList<FutureMeeting>();
+        futuremeetings.add(f1);
+        futuremeetings.add(f2);
     }
 
 	@After
 	public void cleanUp() {
 	}
 
+	@Test
+	public void testsAddAndGetContacts() {
+		data.addContacts(contacts);
+		Set<Contact> returned = data.getContacts();
+		
+		assertTrue(returned.containsAll(contacts));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testsAddNullContacts() {
+		Set<Contact> null_contacts = null;
+		data.addContacts(null_contacts);
+	}
+	
+	@Test
+	public void testsAddAndGetPastMeetings() {
+		data.addPastMeetings(pastmeetings);
+		List<PastMeeting> returned = data.getPastMeetings();
+		
+		assertTrue(returned.containsAll(pastmeetings));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testsAddNullPastMeetings() {
+		List<PastMeeting> null_meetings = null;
+		data.addPastMeetings(null_meetings);
+	}
+
+	@Test
+	public void testsAddAndGetFutureMeetings() {
+		data.addFutureMeetings(futuremeetings);
+		List<FutureMeeting> returned = data.getFutureMeetings()();
+		
+		assertTrue(returned.containsAll(futuremeetings));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testsAddNullFutureMeetings() {
+		List<FutureMeeting> null_meetings = null;
+		data.addFutureMeetings(null_meetings);
+	}
+	
 	@Test
 	public void testsLoadFile() {
 		fail("Not yet implemented");
@@ -72,17 +123,7 @@ public class DataUtilitiesXmlImplTest {
 	}
 	
 	@Test
-	public void testsAddContacts() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
 	public void testsGetContacts() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testsAddPastMeetings() {
 		fail("Not yet implemented");
 	}
 	
@@ -91,11 +132,6 @@ public class DataUtilitiesXmlImplTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testsAddFutureMeetings() {
-		fail("Not yet implemented");
-	}
-	
 	@Test
 	public void testsGetFutureMeetings() {
 		fail("Not yet implemented");
