cc9049196094053c3d6afc8a56664a21260ef76d
Simon Ward <simon.ward42@gmail.com>
2013-02-18 13:02:51 +0000

    Added the final ContactManager test, testFlushAndLoad(): calls flush after previous tests have added contacts & meetings, reloads from the file by calling new ContactManagerImpl(), then re-runs the tests of getter methods. As all tests now successfully pass, this completes the project!

--- test/main/ContactManagerImplTest.java
+++ test/main/ContactManagerImplTest.java
@@ -294,108 +294,21 @@ public class ContactManagerImplTest {
 		cm.getContacts(nullStr);
 	}
 
-	/**
-	 * Test method for {@link main.ContactManagerImpl#flush()}.
-	 */
 	@Test
-	public final void testFlush() {
-		fail("Not yet implemented"); // TODO
+	public final void testFlushAndLoad() {
+		cm.flush();
+		
+		// Reload data from file into contact manager
+		cm = new ContactManagerImpl();
+		
+		// Rerun tests for getters
+		testGetPastMeeting();
+		testGetFutureMeeting();
+		testGetMeeting();
+		testGetFutureMeetingListContact();
+		testGetFutureMeetingListCalendar();
+		testGetPastMeetingList();
+		testGetContactsVarArgs();
+		testGetContactsNameSearch();
 	}
-
-	/**
-	 * Test method for {@link java.lang.Object#Object()}.
-	 */
-	@Test
-	public final void testObject() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#getClass()}.
-	 */
-	@Test
-	public final void testGetClass() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#hashCode()}.
-	 */
-	@Test
-	public final void testHashCode() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
-	 */
-	@Test
-	public final void testEquals() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#clone()}.
-	 */
-	@Test
-	public final void testClone() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#toString()}.
-	 */
-	@Test
-	public final void testToString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#notify()}.
-	 */
-	@Test
-	public final void testNotify() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#notifyAll()}.
-	 */
-	@Test
-	public final void testNotifyAll() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#wait(long)}.
-	 */
-	@Test
-	public final void testWaitLong() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#wait(long, int)}.
-	 */
-	@Test
-	public final void testWaitLongInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#wait()}.
-	 */
-	@Test
-	public final void testWait() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link java.lang.Object#finalize()}.
-	 */
-	@Test
-	public final void testFinalize() {
-		fail("Not yet implemented"); // TODO
-	}
-
 }
